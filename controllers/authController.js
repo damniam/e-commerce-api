356b0d367f06fedff7260df8f2a7d74584face4e
Damian Dzialo <ddzialo28@interia.pl>
2022-02-01 23:45:43 +0100

    Handle hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,42 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please enter a email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError(
+      "Please provide correct password"
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "User successfully login" });
 };
 const register = async (req, res) => {
-    console.log(req.body);
-    await User.create(req.body);
-    res.send("register user");
+  const { name, email, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user, msg: "User created successfully" });
 };
-const logout = async (req, res) => {
-  res.send("logout user");
-};
+
+const logout = async (req, res) => {};
 
 module.exports = { login, register, logout };
